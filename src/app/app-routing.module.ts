97d3247c11bb3a6aad6c9eee4b398df317a6a19a
Siegfried Boeken <37549326+VarVergErgos@users.noreply.github.com>
2023-01-29 11:39:06 +0100

    create the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AddStudentComponent} from './components/add-student/add-student.component'
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { ListStudentComponent } from './components/list-student/list-student.component';
+import { LoginStudentComponent } from './components/login-student/login-student.component';
+import { RegistrationComponent } from './components/registration/registration.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: AddStudentComponent },
+  { path: 'list', component: ListStudentComponent },
+  { path: 'edit', component: EditStudentComponent },
+  { path: 'login', component: LoginStudentComponent },
+  { path: 'register', component: RegistrationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
