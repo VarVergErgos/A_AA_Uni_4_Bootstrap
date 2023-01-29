5ce1a436d8ada5ca786c9de043721bfbdc021176
Siegfried Boeken <37549326+VarVergErgos@users.noreply.github.com>
2023-01-29 10:09:10 +0100

    ng g c components/add-student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentComponent
   ],
   imports: [
     BrowserModule,
